fe08754d2b1a591a7218aeea589455b30ae2661b
Saakethram <96392753+Saakethram@users.noreply.github.com>
2023-12-11 23:38:32 +0530

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,24 @@
+def calculate_bmi(weight, height):
+    bmi = weight / (height ** 2)
+    return bmi
+
+def classify_bmi(bmi):
+    if bmi < 18.5:
+        return "Underweight"
+    elif 18.5 <= bmi < 25:
+        return "Normal weight"
+    elif 25 <= bmi < 30:
+        return "Overweight"
+    else:
+        return "Obese"
+
+def main():
+    weight = float(input("Enter your weight in kilograms: "))
+    height = float(input("Enter your height in meters: "))
+
+    user_bmi = calculate_bmi(weight, height)
+    bmi_category = classify_bmi(user_bmi)
+
+    print(f"Your BMI is: {user_bmi:.2f}")
+    print(f"Category: {bmi_category}")
+main()
